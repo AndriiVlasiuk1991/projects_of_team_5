c0f3e5abeac5ac51bf4ca828e3bd8ea051357f15
malychokd <127889510+malychokd@users.noreply.github.com>
2023-07-09 00:49:59 +0300

    Додав редагування та видалення нотаток

--- Pet_shop_customers.py
+++ Pet_shop_customers.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from sorter import sort_file
+from note_book import magic_8_ball, note_add, note_find, note_show_all, note_remove, note_edite
 
 def main():
     print('Вітаю!\n') #ToDo Потрібно дати коротенький опис програми, зробим в самому кінці
@@ -10,7 +11,25 @@ def main():
     if command == '1':
         pass
     elif command == '2':
-        pass
+        print('1 - Для створення нової нотатки\n2 - Для пошуку по нотаткам\n3 - Вивести всі нотатки\n4 - Видалити нотатку по назві\n5 - Редагувати нотату')
+        commandNote = input('Оберіть команду: ')
+        if commandNote == '1':
+            note_add()
+        elif commandNote == '2':
+            result = note_find()
+            if result == '':
+                print('Нічого не знайдено.')
+            else:
+                print(result)  
+        elif commandNote == '3':
+            print(note_show_all())
+        elif commandNote == '4':
+            note_remove()
+        elif commandNote == '5':
+            note_edite()
+        else:
+            print('Команда невідома')
+              
     elif command == '3':
         list_error_files = []
         while True:
