2cafd4397f2468628fcff7490b2fb71df84b9d0a
9-Bogdan <127949078+9-Bogdan@users.noreply.github.com>
2023-07-10 13:20:14 +0300

    Видалив зайвий файл, додав текст у файл address_book.py та Pet_shop_customers.py

--- Pet_shop_customers.py
+++ Pet_shop_customers.py
@@ -1,39 +1,35 @@
 import sys
 from sorter import sort_file
 from note_book import magic_8_ball, note_add, note_find, note_show_all, note_remove, note_edite
+from address_book import main_addressbook
+
 
 def main():
-    print('Вітаю!\n') #ToDo Потрібно дати коротенький опис програми, зробим в самому кінці
-    print('1 - Книга клієнтів\n2 - Нотатки\n3 - Сортувати файли\n') #ToDo Підправити текст
-    command = input('Оберіть команду: ')
+    print('Вітаю!')
+    while True:
+        print("\n--------------- Menu ---------------\n")
+        print('Ви у головному меню!\nДоступні команди:')
+        print('1 - Книга клієнтів\n2 - Нотатки\n3 - Сортувати файли\n4 - Вихід')
+        command = input('Оберіть команду: ')
 
-    if command == '1':
-        pass
-    elif command == '2':
-        print('1 - Для створення нової нотатки\n2 - Для пошуку по нотаткам\n3 - Вивести всі нотатки\n4 - Видалити нотатку по назві\n5 - Редагувати нотату')
-        commandNote = input('Оберіть команду: ')
-        if commandNote == '1':
-            note_add()
-        elif commandNote == '2':
-            result = note_find()
-            if result == '':
-                print('Нічого не знайдено.')
-            else:
-                print(result)  
-        elif commandNote == '3':
-            print(note_show_all())
-        elif commandNote == '4':
-            note_remove()
-        elif commandNote == '5':
-            note_edite()
+        if command == '1':
+            print("\nВи обрали команду під номером 1. Отже, вітаємо в книзі клієнтів!")
+            main_addressbook()
+            print("Ви завершили роботу в книзі клієнтів!")
+        elif command == '2':
+            print("\nВи обрали команду під номером 2. Отже, вітаємо в нотатках!")
+
+            print("Ви завершили роботу в нотатках!")
+        elif command == '3':
+            print("\nВи обрали команду під номером 3 - 'Сортування папки'!")
+            sort_file()
+            print("Сортування папки завершено!")
+        elif command == '4':
+            print("\nДо зустрічі!")
+            break
         else:
-            print('Команда невідома')
-              
-    elif command == '3':
-        sort_file()
-    else:
-        pass
-
+            print("Такої команди не існує! Оберіть команду з вище перелічених!")
+            print("------------ Команди не існує ------------\n")
 
 
 if __name__ == '__main__':
